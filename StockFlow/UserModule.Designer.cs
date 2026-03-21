2090179fb5a109dc0489f4807e971e2630a9bd66
NguyenDucDuy <nguyenducduy03042006@gmail.com>
2026-03-21 21:35:48 +0700

    Submit thiết kế giao diện - NguyenDucDuy

---
+++ StockFlow/UserModule.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace StockFlow
+{
+    partial class UserModule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUserModule = new System.Windows.Forms.Label();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblPassWord = new System.Windows.Forms.Label();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.btnX);
+            this.panel1.Controls.Add(this.lblUserModule);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(788, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblUserModule
+            // 
+            this.lblUserModule.AutoSize = true;
+            this.lblUserModule.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserModule.ForeColor = System.Drawing.Color.Black;
+            this.lblUserModule.Location = new System.Drawing.Point(12, 18);
+            this.lblUserModule.Name = "lblUserModule";
+            this.lblUserModule.Size = new System.Drawing.Size(120, 16);
+            this.lblUserModule.TabIndex = 0;
+            this.lblUserModule.Text = "USER  MODULE";
+            // 
+            // btnX
+            // 
+            this.btnX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnX.BackColor = System.Drawing.Color.Red;
+            this.btnX.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnX.ForeColor = System.Drawing.Color.White;
+            this.btnX.Location = new System.Drawing.Point(765, 0);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(23, 23);
+            this.btnX.TabIndex = 27;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(158)))), ((int)(((byte)(235)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Location = new System.Drawing.Point(466, 321);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 24);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(124)))), ((int)(((byte)(195)))));
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Location = new System.Drawing.Point(373, 321);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 24);
+            this.btnUpdate.TabIndex = 21;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(168)))), ((int)(((byte)(220)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Location = new System.Drawing.Point(273, 321);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 24);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.txtPhone.Location = new System.Drawing.Point(261, 254);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(300, 22);
+            this.txtPhone.TabIndex = 19;
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.txtPassWord.Location = new System.Drawing.Point(261, 205);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(300, 22);
+            this.txtPassWord.TabIndex = 18;
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.txtFullName.Location = new System.Drawing.Point(261, 152);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(300, 22);
+            this.txtFullName.TabIndex = 17;
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.txtName.Location = new System.Drawing.Point(261, 101);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(300, 22);
+            this.txtName.TabIndex = 16;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(122, 257);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(52, 16);
+            this.lblPhone.TabIndex = 15;
+            this.lblPhone.Text = "Phone :";
+            // 
+            // lblPassWord
+            // 
+            this.lblPassWord.AutoSize = true;
+            this.lblPassWord.Location = new System.Drawing.Point(122, 208);
+            this.lblPassWord.Name = "lblPassWord";
+            this.lblPassWord.Size = new System.Drawing.Size(77, 16);
+            this.lblPassWord.TabIndex = 14;
+            this.lblPassWord.Text = "PassWord :";
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(122, 155);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(74, 16);
+            this.lblFullName.TabIndex = 13;
+            this.lblFullName.Text = "Full Name :";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(122, 104);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(82, 16);
+            this.lblUserName.TabIndex = 12;
+            this.lblUserName.Text = "User Name :";
+            // 
+            // UserModule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(788, 510);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtPassWord);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblPassWord);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserModule";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UserModule";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblUserModule;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblPassWord;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label lblUserName;
+    }
+}
--- StockFlow/UserModule.cs
+++ StockFlow/UserModule.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StockFlow
 {
-    public partial class Form1 : Form
+    public partial class UserModule : Form
     {
-        public Form1()
+        public UserModule()
         {
             InitializeComponent();
         }
